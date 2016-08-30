0cbcf051239d19e2948556d8b5fb801777463f4c
leonardo <l.castillo1989@Hotmail.com>
2016-08-29 21:13:37 -0400

    refactored code

--- isTriangle.rb
+++ isTriangle.rb
@@ -4,16 +4,29 @@
 
 def is_triangle(side1,side2,side3)
   sides = [side1,side2,side3]
+  unique_sides = sides.uniq.length
+  raise TriangleError if triangle_error_found?(sides)
+  type_of_triangle(unique_sides)
+end
+
+def type_of_triangle(nous)          #nous stands for number of unique sides
+  return 'Equilateral' if nous == 1
+  return 'Isosceles' if nous == 2
+  return 'Scalene' if nous == 3
+end
+
+def triangle_error_found?(sides)
   biggest_side = sides.max
-  sides.each do |side|
-    if side <= 0
-     raise TriangleError
-    end
-  end
-  raise TriangleError if sides.inject(:+) - biggest_side < biggest_side
-  return 'Equilateral' if sides.uniq.length == 1
-  return 'Isosceles' if sides.uniq.length == 2
-  return 'Scalene' if sides.uniq.length == 3
+  return true if any_sides_smaller_than_zero?(sides) || bigger_side_valid?(sides,biggest_side)
+  return false
+end
+
+def any_sides_smaller_than_zero?(sides)
+  sides.any? { |side| side <= 0 }
+end
+ 
+def bigger_side_valid?(sides,biggest_side)
+  sides.inject(:+) - biggest_side < biggest_side 
 end
 
 class TriangleError < RuntimeError
